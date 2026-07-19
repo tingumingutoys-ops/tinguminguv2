d4b69286e57746f07d075244b3ac25f075437bc6
tingu mingu <tingumingutoys@gmail.com>
2026-07-19 22:29:13 +0500

    update website

--- src/App.tsx
+++ src/App.tsx
@@ -16,10 +16,8 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
+          <h1>Tingu Mingu</h1>
+        
         </div>
         <button
           type="button"
