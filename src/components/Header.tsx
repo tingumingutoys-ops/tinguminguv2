b5241dcf347e4f727e49b5103a6168eac458403c
tingu mingu <tingumingutoys@gmail.com>
2026-07-20 02:50:36 +0500

    ne

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,36 +6,42 @@ function Header() {
   return (
     <header className="header">
 
-      <div className="logo">
-        🧸 Tingu Mingu
+      <div className="top-header">
+
+        <div className="logo">
+          🧸 Tingu Mingu
+        </div>
+
+        <div className="search-box">
+          <input
+            type="text"
+            placeholder="Search products..."
+          />
+        </div>
+
+        <div className="header-buttons">
+
+          <button className="loginBtn">
+            Login
+          </button>
+
+          <button className="cartBtn">
+            🛒 ({cart.length})
+          </button>
+
+        </div>
+
       </div>
 
-      <nav className="nav">
+      <nav className="navbar">
         <a href="#">Home</a>
         <a href="#">Shop</a>
         <a href="#">Men</a>
         <a href="#">Women</a>
         <a href="#">Kids</a>
+        <a href="#">Toys</a>
         <a href="#">Contact</a>
       </nav>
-
-      <div className="right">
-
-        <input
-          className="search"
-          type="text"
-          placeholder="Search products..."
-        />
-
-        <button className="cartBtn">
-          🛒 {cart.length}
-        </button>
-
-        <button className="loginBtn">
-          Login
-        </button>
-
-      </div>
 
     </header>
   );
